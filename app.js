d20f872670a3dba382c85ea53aced990bc3301f0
Pohfy123 <pohfy123@gmail.com>
2016-12-08 00:12:55 +0700

    reset code

--- app.js
+++ app.js
@@ -12,10 +12,8 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 // app.use(bodyParser.json({ verify: verifyRequestSignature }));
-app.use(bodyParser.urlencoded({
-  limit: "50mb", parameterLimit: 50000
-}));
-app.use(bodyParser.json({limit: '50mb', type: 'application/json'}));
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 var MongoClient = require('mongodb').MongoClient
@@ -190,7 +188,6 @@ app.post('/addFacebookId', function (req, res) {
   var senderId = req.body.senderId;
   var facebookId = req.body.facebookId;
   var facebook = req.body.facebook;
-  console.log(req.body)
   facebook.senderID = facebookId
   var user = {
     senderID: senderId,
