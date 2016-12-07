eea94b2a693ab9ab0fe2ebbab29e20320d433aba
Pohfy123 <pohfy123@gmail.com>
2016-12-08 00:09:00 +0700

    fix

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 // app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(bodyParser.urlencoded({
-  limit: "50mb", parameterLimit: 50000, type:'application/x-www-form-urlencoding'
+  limit: "50mb", parameterLimit: 50000
 }));
 app.use(bodyParser.json({limit: '50mb', type: 'application/json'}));
 app.use(express.static('public'));
