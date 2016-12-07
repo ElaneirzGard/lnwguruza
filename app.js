e74292fcb9c0027fb8b33bcf530df50c24d43cfb
ElaneirzGard <elaneirzgard@gmail.com>
2016-12-07 22:30:28 +0700

    simsimi thai

--- app.js
+++ app.js
@@ -612,6 +612,7 @@ function receivedMessage(event) {
         else{// simsimi
             //simsimi
             var text = messageText;
+                console.log("-----messageText : ".concat(messageText));
             var thaiChar = ['ก','ข','ฃ','ค','ฅ','ฆ','ง','จ','ฉ','ช','ซ','ฌ','ญ','ฎ','ฏ','ฐ','ฑ','ฒ','ณ','ด','ต','ถ','ท','ธ','น','บ','ป','ผ','ฝ','พ','ฟ','ภ','ม','ย','ร','ล','ว','ศ','ษ','ส','ห','ฬ','อ','ฮ'];
             var isThai = false;
                 console.log("---- check thai lang ----");
