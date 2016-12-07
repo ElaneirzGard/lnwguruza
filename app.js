0c5d2edacc4928dc55df9163db8d031fe7d997af
Pohfy123 <pohfy123@gmail.com>
2016-12-08 00:02:56 +0700

    fix

--- app.js
+++ app.js
@@ -190,6 +190,7 @@ app.post('/addFacebookId', function (req, res) {
   var senderId = req.body.senderId;
   var facebookId = req.body.facebookId;
   var facebook = req.body.facebook;
+  console.log(facebook)
   facebook.senderID = facebookId
   var user = {
     senderID: senderId,
