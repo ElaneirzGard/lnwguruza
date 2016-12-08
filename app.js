313c6c027d6e6e6ee7afd5b463f96e8d316552e9
Pohfy123 <pohfy123@gmail.com>
2016-12-08 22:30:20 +0700

    change button

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.get('/loginfb/:senderId', function(req, res){
                 <title>ข้ารู้ข้าเห็นข้าเป็นai</title>
               </head>
               <body>
-                  <div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="false" onclick="myFacebookLogin()"></div>
+                  <button onclick="myFacebookLogin()">Login with Facebook</button>
               </body>
             </html>
 
